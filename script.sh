ca7ab1dbf4ba018f98745ba9627b435b8b6453b7
vsm <devops.july2.2017@gmail.com>
2023-06-07 12:38:29 +0530

    Update script.sh

--- script.sh
+++ script.sh
@@ -1,1 +1,25 @@
-docker run -itd --name=web -p 9000:80 nginx
+#docker run -itd --name=web -p 9000:80 nginx
+
+#!/bin/bash
+read -p "Enter file name : " filename
+while read line
+do 
+if [[ "$line" == *"services:"* ]]; then
+  continue
+fi
+if [[ "${line:0-1}" == ":" ]]; then
+        cname=$line
+	echo $cname
+	continue
+fi
+key=`echo $line | aws -F: '{print $1}'`
+value=`echo $line | aws -F: '{print $2}'`
+
+if [[ "${key}" == "image:" ]]; then
+        image=$value
+fi
+if [[ "${key}" == "port:" ]]; then
+        port=$value
+fi
+docker run -itd --name=$cname -p $port $image
+done < $filename
